009d4a2a07780170ab1466fe5899dba0b918b62e
Alejandro Esquivel <yoquiale@gmail.com>
2021-05-05 18:41:15 +0200

    Commit inicial - Barra de navegación en curso

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './modules/app-routing.module';
 
-import { AppComponent } from './app.component';
+import { AppComponent } from './components/app/app.component';
+import { NavComponent } from './components/nav/nav.component';
+import { HomeComponent } from './components/home/home.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, NavComponent, HomeComponent],
+  imports: [BrowserModule, AppRoutingModule, FontAwesomeModule, RouterModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/modules/app-routing.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from '@components/home/home.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/inicio',
+    pathMatch: 'full',
+  },
+  {
+    path: 'inicio',
+    component: HomeComponent,
+  },
+];
+
+@NgModule({
+  declarations: [],
+  imports: [CommonModule, RouterModule.forRoot(routes)],
+})
+export class AppRoutingModule {}
